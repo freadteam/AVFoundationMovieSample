072e771e5b25fd42241de335f9b3647b29dcdc3f
freadteam <freadteam@gmail.com>
2018-07-12 04:18:49 +0900

    first commit

---
+++ MovieSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieSample
+//
+//  Created by Ryo Endo on 2018/07/11.
+//  Copyright © 2018年 Ryo Endo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
